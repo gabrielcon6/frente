d4707fba2763824019fd5d005470004fede4a576
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-04-07 15:23:50 +1000

    about and footer content updated

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,8 +3,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>Hi, I am your Footer.</div>
-    )
+        <section id="contact">
+            <div style={{height: "100vh"}}>
+                <div class="address">
+                    <p>phone: 0434 774 792</p>
+                    <p>address: Rua Sem Fim, n 24</p>
+                    <p>address: dev@frente.com.au</p>
+                </div>
+                <div class="icons-contact">
+                    <p>LinkedIn Link</p>
+                    <p>Facebook Link?</p>
+                    <p>Email Link?</p>
+                </div>
+            </div>
+        </section>    )
 }
 
 export default Footer;
